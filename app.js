80540fbe537ec72e641065f150c42ff2aa33feae
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-05-05 00:04:46 +0530

    this is making ob scroll fiunction

--- app.js
+++ app.js
@@ -24,6 +24,31 @@ function navMenu() {
             navbar.classList.remove("navbar-sticky-on");
         }
     }
-}
 
+}
 navMenu()
+
+document.addEventListener("DOMContentLoaded", ()=> {
+   function counter(id,start,end,duration){
+    let obj =document.getElementById(id),
+    current=start,
+    range=end-start,
+    incerement=end>start?1:-1,
+    step =Math.abs(Math.floor(duration/range)),
+    timer=setInterval(()=>{
+        current+= incerement;
+        obj.textContent=current;
+        if
+        (current==end)
+        {
+            clearInterval(timer);
+        }
+    },step)
+    
+   }
+   counter("count1",0,10,2000);
+   counter("count2",100,10,2000);
+   counter("count3",0,10,4000);
+   counter("count4",0,10,2000);
+});
+
