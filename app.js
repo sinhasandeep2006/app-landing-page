706ae6f880879b4a9312c55ddd132824c5547f36
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-05-05 13:32:48 +0530

    this is promo part

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-function navMenu() {
-    let navbar = document.querySelector('.navbar-sticky');
-    let scrollTopButton = document.querySelector('#scrollUp');
-
-    window.onscroll = function () {
+function navMenu(){
+    let navBar = document.querySelector('.navbar-sticky'); 
+    let scrollTopButton = document.querySelector("#scrollUp"); 
+    window.onscroll = function () { 
         var scroll = document.documentElement.scrollTop;
         if (scroll >= 120) {
-            navbar.classList.add("navbar-sticky-moved-up");
-        }
-        else {
-            navbar.classList.remove("navbar-sticky-moved-up");
-        }
-        if (scroll >= 250) {
-            navbar.classList.add("navbar-sticky-transitioned");
-            scrollTopButton.classList.add("scrollActive");
+            navBar.classList.add("navbar-sticky-moved-up");
         } else {
-            navbar.classList.remove("navbar-sticky-transitioned");
-            scrollTopButton.classList.remove("scrollActive");
+            navBar.classList.remove("navbar-sticky-moved-up");
         }
-        if (scroll>=500) {
-            navbar.classList.add("navbar-sticky-on");
+        // apply transition
+        if (scroll >= 250) {
+            navBar.classList.add("navbar-sticky-transitioned");
+            scrollTopButton.classList.add('scrollActive');
+        } else {
+            navBar.classList.remove("navbar-sticky-transitioned");
+            scrollTopButton.classList.remove('scrollActive');
         }
-        else{
-            navbar.classList.remove("navbar-sticky-on");
+        // sticky on
+        if (scroll >= 500) {
+            navBar.classList.add("navbar-sticky-on");
+        } else {
+            navBar.classList.remove("navbar-sticky-on");
         }
     }
-
 }
-navMenu()
+navMenu();
+
+
 
 document.addEventListener("DOMContentLoaded", ()=> {
    function counter(id,start,end,duration){
