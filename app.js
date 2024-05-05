3875bca3f2168f389e5a0c0abbab2785839219cf
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-05-05 13:51:38 +0530

    this is the comminting of the scroll active

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ function navMenu(){
         // apply transition
         if (scroll >= 250) {
             navBar.classList.add("navbar-sticky-transitioned");
-            scrollTopButton.classList.add('scrollActive');
+            // scrollTopButton.classList.add('scrollActive');
         } else {
             navBar.classList.remove("navbar-sticky-transitioned");
-            scrollTopButton.classList.remove('scrollActive');
+            // scrollTopButton.classList.remove('scrollActive');
         }
         // sticky on
         if (scroll >= 500) {
