4dc459930127fd62b39eaa157d3c36aa6c3a5ea8
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-05-16 18:00:34 +0530

    this the secreen shot feature

--- app.js
+++ app.js
@@ -52,3 +52,29 @@ document.addEventListener("DOMContentLoaded", ()=> {
    counter("count4",0,10,2000);
 });
 
+
+
+var swiper = new Swiper( '.swiper-container.app-screen', {
+    effect: 'coverflow',
+    loop: true,
+    autoplaySpeed: 3000,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    paginationClickable: true,
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows : false,
+    }
+});  
+  
